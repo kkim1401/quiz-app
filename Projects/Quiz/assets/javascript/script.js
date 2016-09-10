023a13dea888987b30daadbc31c9414ad2a102e7
Kevin Kim <ykk3416@gmail.com>
2016-09-10 01:33:15 -0400

    Adopted MVC and cleaned up some unnecessary logic.

--- Projects/Quiz/assets/javascript/script.js
+++ Projects/Quiz/assets/javascript/script.js
@@ -7,7 +7,6 @@ function Question(question, answerChoices, correctIndex) {
     this.answerChoices = answerChoices;
     this.correctIndex = correctIndex;
     this.correct = false;
-    //this.answered = false; //Will replace answered boolean and removeAnsweredQuestion method with simple popping of first item of array after next is clicked.
 }
 
 Question.prototype = {
@@ -20,7 +19,6 @@ Question.prototype = {
     }
 };
 
-//Implementing Dan Martensen's basic Subject template for MVC.
 function Subject() {
     var observers = [];
     return {
@@ -38,125 +36,122 @@ function Subject() {
     }
 }
 
-var questions = function() {
+function QuestionsModel() {
     var subject = Subject(),
         questions = [],
         numberCorrect = 0,
         totalNumber = 0;
+
+    //QuestionsModel object
     return {
-        getFirstQuestion: function() {
+        getFirstQuestion: function () {
             return questions[0];
         },
-        getQuestionsLength: function() {
-          return questions.length;
+        getQuestionsLength: function () {
+            return questions.length;
         },
-        addQuestion: function(question) {
+        addQuestion: function (question) {
             questions.push(question);
+            subject.notifyObservers();
         },
-        removeQuestion: function() {
+        removeQuestion: function () {
             questions.shift();
+            subject.notifyObservers();
         },
-        getTotalNumber: function() {
+        getTotalNumber: function () {
             return totalNumber;
         },
-        getNumberCorrect: function() {
+        getNumberCorrect: function () {
             return numberCorrect;
         },
-        calcTotalNumber: function() {
+        calcTotalNumber: function () {
             totalNumber = questions.length;
+            subject.notifyObservers();
         },
-        calcNumberCorrect: function(question) {
+        calcNumberCorrect: function (question) {
             if (question.correct) {
                 numberCorrect++;
             }
         },
-        register: function() {
+        register: function (args) {
             subject.removeAll();
-            arguments.forEach(function(observer){
-                subject.add(observer);
-            });
+            for (var i = 0; i < arguments.length; i++) {
+                subject.add(arguments[i]);
+            }
         }
-        };
-}();
+    }
+}
 
-var handler = {
-    initialize: function(args) {
-        for (var i = 0; i < arguments.length; i++) {
-            questions.addQuestion(arguments[i]);
-        }
-        questions.calcTotalNumber();
-        this.display();
-        this.setUpListener();
-    },
-    display: function() {
-        view.render(questions.getFirstQuestion());
-    },
-    setUpListener: function() {
-        var form = document.forms[0];
-        form.addEventListener("submit", function(event) {
-            event.preventDefault();
-            if (document.getElementsByTagName("fieldset")[0].hasChildNodes()) {
-                console.log("in if statement of analyze");
-                var choices = document.getElementsByName("choices");
-                var question = questions.getFirstQuestion();
-                for (var i = 0; i < choices.length; i++) {
-                    if (choices[i].checked) {
-                        question.evaluate(i);
-                        questions.calcNumberCorrect(question);
-                        questions.removeQuestion();
+function Handler(view, model) {
+    return {
+        notify: function () {
+            var DOM = view.getDOM();
+            DOM.form.addEventListener("submit", function (event) { //remove this handler later
+                event.preventDefault();
+                if (DOM.fieldset.hasChildNodes()) {
+                    var question = model.getFirstQuestion();
+                    for (var i = 0; i < DOM.choices.length; i++) {
+                        if (DOM.choices[i].checked) {
+                            question.evaluate(i);
+                            model.calcNumberCorrect(question);
+                            model.removeQuestion();
+                        }
                     }
                 }
-                view.wipe();
-                if(questions.getQuestionsLength()) {
-                    handler.display();
-                }
-                else {
-                    handler.conclude();
-                }
-            }
-            else {
-                this.conclude();
-            }
-        }, false);
-    },
-    conclude: function() {
-        console.log("You got "+ questions.getNumberCorrect() + " out of " + questions.getTotalNumber());
-    }
+            }, false);
+        }
+    };
+}
 
-};
-
-var view = function() {
-    var fieldset = document.body.getElementsByTagName("fieldset")[0];
+function View(model) {
+    var DOM = {
+        form: document.forms[0],
+        fieldset: document.getElementsByTagName("fieldset")[0],
+        choices: document.getElementsByName("choices")
+    };
     return {
-        render: function (question) {
-            console.log("view.render activates!");
+        getDOM: function() {
+            return DOM;
+    },
+        notify: function () {
+            DOM.fieldset.innerHTML = "";
+            if (model.getQuestionsLength()) {
             var input,
                 label,
                 docFrag = document.createDocumentFragment(),
-                label2 = document.createElement("label"),
-                labelQuestion = document.createTextNode(question.question);
-            label2.appendChild(labelQuestion);
-            docFrag.appendChild(label2);
-            for (var i = 0; i < question.answerChoices.length; i++) {
+                labelForQuestion = document.createElement("label"),
+                labelQuestion = document.createTextNode(model.getFirstQuestion().question);
+            labelForQuestion.appendChild(labelQuestion);
+            docFrag.appendChild(labelForQuestion);
+            for (var i = 0; i < model.getFirstQuestion().answerChoices.length; i++) {
                 input = document.createElement("input");
                 label = document.createElement("label");
                 input.type = "radio";
                 input.name = "choices";
-                input.value = question.answerChoices[i];
+                input.value = model.getFirstQuestion().answerChoices[i];
                 label.appendChild(input);
                 docFrag.appendChild(label);
                 input.insertAdjacentHTML("afterend", input.value);
             }
-            fieldset.appendChild(docFrag);
-        },
-        wipe: function () {
-            fieldset.innerHTML = "";
-        },
-        conclude: function() {
-
+            DOM.fieldset.appendChild(docFrag);
+            }
+            else {
+            console.log("You got "+ model.getNumberCorrect() + " out of " + model.getTotalNumber());
+        }
         }
     };
-}();
+}
 
-handler.initialize(new Question("What is your name?", ["Kevin", "Zoe"], 0));
+
+//Initialization
+var model = QuestionsModel(),
+    view = View(model),
+    handler = Handler(view, model);
+
+model.register(handler, view);
+model.addQuestion(new Question("What is your name?", ["Kevin", "Zoe"], 0));
+
+
+
+
 
